de863aa7f7eb90da0bdd9bd68230c9bb4b18db69
João <jomaatheus@gmail.com>
2024-06-26 18:22:08 -0300

    alterar

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
-export function menuShow() {
+function menuShow() {
   let menuMobile = document.querySelector(".nav-menu-mobile ul");
 
-  if (menuMobile.classList.contains("active")) {
-    menuMobile.classList.remove("active");
+  if (menuMobile.classList.contains('active')) {
+    menuMobile.classList.remove('active');
     document.querySelector(".menu-icon img ").src = "images/menu.png";
   } else {
     menuMobile.classList.add("active");
